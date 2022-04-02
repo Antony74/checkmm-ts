e0642eb494d13dfee7f6aa7f7cb024dc7c475d26
Antony Bartlett <antony.bartlett@ukri.org>
2022-04-02 21:05:58 +0100

    Add some code to checkmm.ts with which to get started with some transpilation.

---
+++ ts/checkmm.ts
@@ -0,0 +1,28 @@
+import { std } from "./helpers";
+
+type Expression = Array<string>;
+
+// The first parameter is the statement of the hypothesis, the second is
+// true iff the hypothesis is floating.
+type Hypothesis = std.Pair<Expression, boolean>;
+
+const hypotheses = new Map<string, Hypothesis>();
+
+// An axiom or a theorem.
+interface Assertion
+{
+    // Hypotheses of this axiom or theorem.
+    hypotheses: std.deque<string>;
+    disjvars: Set<std.Pair<string, string> > ;
+    // Statement of axiom or theorem.
+    expression: Expression;
+};
+
+const assertions = new Map<string, Assertion>();
+
+// Determine if a string is used as a label
+const labelused = (label: string): boolean => {
+  return (
+    hypotheses.get(label) !== undefined || assertions.get(label) != undefined
+  );
+};
