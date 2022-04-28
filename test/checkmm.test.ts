163a7233d6e3fabd9f026bac0dc268d18c702859
Antony Bartlett <antony.bartlett@ukri.org>
2022-04-28 17:59:32 +0100

    getfloatinghyp test

--- test/checkmm.test.ts
+++ test/checkmm.test.ts
@@ -33,38 +33,37 @@ describe('checkmm', () => {
         });
     });
 
-    // it('can find a floating hypothesis', () => {
+    describe('getfloatinghyp', () => {
+        it('can find a floating hypothesis', () => {
+            checkmm.setScopes([
+                {
+                    activevariables: new Set<string>(),
+                    activehyp: [],
+                    disjvars: [],
+                    floatinghyp: new Map<string, string>(
+                        Object.entries({
+                            hello: 'world',
+                        }),
+                    ),
+                },
+            ]);
+
+            expect(checkmm.getfloatinghyp('hello')).toEqual('world');
+            expect(checkmm.getfloatinghyp('other')).toEqual('');
+        });
+    });
+
+    //   it('can get the next token', () => {
 
     //     const checkmm = new CheckMM();
-    
-    //     checkmm.setState({
-    //       scopes: [
-    //         {
-    //           activevariables: new Set<string>(),
-    //           activehyp: [],
-    //           disjvars: [],
-    //           floatinghyp: {
-    //             hello: 'world'
-    //           }
-    //         }
-    //       ]
-    //     });
-    
-    //     expect(checkmm.getfloatinghyp('hello')).to.equal('world');
-    //     expect(checkmm.getfloatinghyp('other')).to.equal('');
-    //   });
-    
-    //   it('can get the next token', () => {
-    
-    //     const checkmm = new CheckMM();
-    
+
     //     const old = console.error;
     //     let errors = 0;
     //     console.error = () => ++errors;
-    
+
     //     let input = 'hello world';
     //     let token = '';
-    
+
     //     ({token, input} = checkmm.nexttoken(input));
     //     expect(token).to.equal('hello');
     //     ({token, input} = checkmm.nexttoken(input));
@@ -74,26 +73,26 @@ describe('checkmm', () => {
     //     ({token, input} = checkmm.nexttoken(input));
     //     expect(token).to.equal('');
     //     expect(errors).to.equal(0);
-    
+
     //     input = String.fromCharCode(127);
     //     ({token, input} = checkmm.nexttoken(input));
     //     expect(token).to.equal('');
     //     expect(errors).to.equal(1);
-    
+
     //     console.error = old;
     //   });
-    
+
     //   it('can read tokens', () => {
     //     const checkmm = new CheckMM();
     //     const okay: boolean = checkmm.readtokens(__dirname + '/../../node_modules/metamath-test/anatomy.mm');
     //     expect(okay).to.equal(true);
     //     expect(checkmm.tokens.length).to.equal(60);
     //   });
-    
+
     //   it('can construct assertions with disjoint variables', () => {
-    
+
     //     const checkmm = new CheckMM();
-    
+
     //     checkmm.setState({
     //       hypotheses: {
     //         wph: {
@@ -127,11 +126,11 @@ describe('checkmm', () => {
     //     expect(assertion.disjvars).to.deep.equal([['ph', 'x']]);
     //     expect(assertion.expression).to.deep.equal(expression);
     //   });
-    
+
     //   it('can read expressions', () => {
-    
+
     //     const checkmm = new CheckMM();
-    
+
     //     checkmm.setState({
     //       tokens: '|- ( ph -> ( ps -> ph ) ) $. $( Axiom _Frege_.'.split(' '),
     //       constants: ['|-', '(', ')', '->', 'ph', 'ps']
@@ -139,11 +138,11 @@ describe('checkmm', () => {
     //     const expression: Expression = checkmm.readexpression('a', 'ax-1', '$.');
     //     expect(expression).to.deep.equal('|- ( ph -> ( ps -> ph ) )'.split(' '));
     //   });
-    
+
     //   it('can make substituions', () => {
-    
+
     //     const checkmm = new CheckMM();
-    
+
     //     const expression: Expression = checkmm.makesubstitution(
     //       ['weather', 'is', 'sunny'],
     //       {
@@ -152,7 +151,7 @@ describe('checkmm', () => {
     //     );
     //     expect(expression).to.deep.equal(['weather', 'is', 'raining', 'cats', 'and', 'dogs']);
     //   });
-    
+
     //   it('can get proof numbers', () => {
     //     const checkmm = new CheckMM();
     //     const proofnumbers: number[] = checkmm.getproofnumbers(
@@ -164,7 +163,7 @@ describe('checkmm', () => {
     //       18, 21, 4, 23, 15, 26, 1, 2, 3, 8, 9, 1, 17, 20, 10, 18, 21, 8, 11, 24, 19, 25, 22, 1, 2, 12, 1, 3, 12, 13, 14
     //     ]);
     //   });
-    
+
     //   it('can verify a proof step references an assertion', () => {
     //     const checkmm = new CheckMM();
     //     checkmm.setState({
@@ -194,7 +193,7 @@ describe('checkmm', () => {
     //         }
     //       }
     //     });
-    
+
     //     const result: Expression[] = checkmm.verifyassertionref('mpdb', 'ax-mp', [
     //       ['wff', 'ps'],
     //       ['wff', 'ch'],
@@ -203,14 +202,14 @@ describe('checkmm', () => {
     //       ['|-', 'ph'],
     //       ['|-', '(', 'ph', '->', 'ps', ')']
     //     ]);
-    
+
     //     expect(result).to.deep.equal([
     //       ['wff', 'ps'],
     //       ['wff', 'ch'],
     //       ['|-', 'ps']
     //     ]);
     //   });
-    
+
     //   it('can verify a proof step references an assertion with disjoint variable conditions', () => {
     //     const checkmm = new CheckMM();
     //     checkmm.setState({
@@ -241,21 +240,21 @@ describe('checkmm', () => {
     //         }
     //       ]
     //     });
-    
+
     //     const result: Expression[] = checkmm.verifyassertionref('a17d', 'ax-17', [
     //       ['wff', '(', 'ps', '->', 'A.', 'x', 'ps', ')'],
     //       ['wff', 'ph'],
     //       ['wff', 'ps'],
     //       ['set', 'x']
     //     ]);
-    
+
     //     expect(result).to.deep.equal([
     //       ['wff', '(', 'ps', '->', 'A.', 'x', 'ps', ')'],
     //       ['wff', 'ph'],
     //       ['|-', '(', 'ps', '->', 'A.', 'x', 'ps', ')']
     //     ]);
     //   });
-    
+
     //   function initStateForTh1(tokens: string[], checkmm: CheckMM) {
     //     const testValues: Partial<State> = {
     //       hypotheses: {
@@ -343,125 +342,122 @@ describe('checkmm', () => {
     //         }
     //       ]
     //     };
-    
+
     //     checkmm.setState(testValues);
     //   }
-    
+
     //   it('can verify regular and compressed proofs', () => {
-    
+
     //     const checkmm = new CheckMM();
-    
+
     //     initStateForTh1([], checkmm);
-    
+
     //     const theorem: Assertion = {
     //       hypotheses: ['tt'],
     //       disjvars: [],
     //       expression: ['|-', 't', '=', 't']
     //     };
-    
+
     //     const proof: string[] = (
     //       'tt tze tpl tt weq tt tt weq tt a2 tt tze tpl ' +
     //       'tt weq tt tze tpl tt weq tt tt weq wim tt a2 ' +
     //       'tt tze tpl tt tt a1 mp mp'
     //     ).split(' ');
-    
+
     //     const result1: boolean = checkmm.verifyregularproof('th1', theorem, proof);
     //     expect(result1).to.equal(true);
     //   });
-    
+
     //   it('can verify compressed proofs', () => {
-    
+
     //     const checkmm = new CheckMM();
-    
+
     //     initStateForTh1([], checkmm);
-    
+
     //     const labels = 'tze tpl weq a2 wim a1 mp'.split(' ');
     //     const proofnumbers = checkmm.getproofnumbers('th1', 'ABCZADZAADZAEZJJKFLIAAGHH');
-    
+
     //     const theorem: Assertion = {
     //       hypotheses: ['tt'],
     //       disjvars: [],
     //       expression: ['|-', 't', '=', 't']
     //     };
-    
+
     //     const result2: boolean = checkmm.verifycompressedproof('th1', theorem, labels, proofnumbers);
     //     expect(result2).to.equal(true);
     //   });
-    
+
     //   it('can parse $p statements for regular proofs', () => {
     //     const checkmm = new CheckMM();
     //     initStateForTh1((
     //       '|- t = t $= tt tze tpl tt weq tt tt weq tt a2 tt tze tpl tt weq tt tze tpl tt weq tt tt weq ' +
     //       'wim tt a2 tt tze tpl tt tt a1 mp mp $.'
     //     ).split(' '), checkmm);
-    
+
     //     const okay: boolean = checkmm.parsep('th1');
     //     expect(okay).to.equal(true);
     //   });
-    
+
     //   it('can parse $p statements for compressed proofs', () => {
     //     const checkmm = new CheckMM();
     //     initStateForTh1((
     //       '|- t = t $= ( tze tpl weq a2 wim a1 mp ) ABCZADZAADZAEZJJKFLIAAGHH $.'
     //     ).split(' '), checkmm);
-    
+
     //     const okay: boolean = checkmm.parsep('th1');
     //     expect(okay).to.equal(true);
     //   });
-    
+
     //   it('can parse $c statements', () => {
     //     const checkmm = new CheckMM();
     //     checkmm.setState({
     //       scopes: [new Scope()],
     //       tokens: '0 + = -> ( ) term wff |- $.'.split(' ')
     //     });
-    
+
     //     const okay = checkmm.parsec();
     //     expect(okay).to.equal(true);
     //   });
-    
+
     //   it('can verify demo0.mm', () => {
-    
+
     //     const old = console.log;
     //     console.log = () => {};
-    
+
     //     const checkmm = new CheckMM();
     //     checkmm.setState({});
-    
+
     //     let okay: boolean = checkmm.readtokens(__dirname + '/../../node_modules/metamath-test/demo0.mm');
     //     expect(okay).to.equal(true);
-    
+
     //     okay = checkmm.checkmm();
     //     expect(okay).to.equal(true);
-    
+
     //     console.log = old;
     //   });
-    
+
     //   it('can asynchronously verify demo0.mm', (done) => {
-    
+
     //     const old = console.log;
     //     console.log = () => {};
-    
+
     //     const url = 'http://loclhost:8080/demo0.mm';
-    
+
     //     const checkmm = new CheckMMex();
-    
+
     //     fetchMock.get('*', fs.readFileSync(__dirname + '/../../node_modules/metamath-test/demo0.mm', {encoding: 'utf8'}));
-    
+
     //     checkmm.readTokensAsync(url, (error: string) => {
     //       expect(error).to.equal('');
     //       expect(checkmm.getState().tokens.length).to.equal(166);
     //       const okay: boolean = checkmm.checkmm();
     //       expect(okay).to.equal(true);
-    
+
     //       console.log = old;
-    
+
     //       done();
     //     });
     //   });
-    
-    // });
-    
-        
 
+    // });
 });
