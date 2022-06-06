b059817866ff1f2bf3bdbdb0aceb7774c9b52274
Antony Bartlett <antony.bartlett@ukri.org>
2022-06-06 19:44:36 +0100

    Added some basic performance monitoring

---
+++ test/performance.ts
@@ -0,0 +1,57 @@
+import checkmm from '../src/checkmm';
+import { performance } from 'perf_hooks';
+
+interface Timing {
+    millisecondsTaken: number;
+    callCount: number;
+}
+
+const timings: { [name: string]: Timing } = {};
+
+const createPerformanceMonitor = <RT, Args extends Array<unknown>>(fn: (...args: Args) => RT, _name?: string) => {
+    const name: string = _name ?? fn.name;
+
+    timings[name] = { millisecondsTaken: 0, callCount: 0 };
+
+    return (...args: Args): RT => {
+        const start = performance.now();
+        const returnValue = fn(...args);
+        const finish = performance.now();
+
+        timings[name].callCount++;
+        timings[name].millisecondsTaken += finish - start;
+
+        return returnValue;
+    };
+};
+
+const createAsyncPerformanceMonitor = <RT, Args extends Array<unknown>>(
+    fn: (...args: Args) => Promise<RT>,
+    _name?: string,
+) => {
+    const name: string = _name ?? fn.name;
+
+    timings[name] = { millisecondsTaken: 0, callCount: 0 };
+
+    return async (...args: Args): Promise<RT> => {
+        const start = performance.now();
+        const returnValue = await fn(...args);
+        const finish = performance.now();
+
+        timings[name].callCount++;
+        timings[name].millisecondsTaken += finish - start;
+
+        return returnValue;
+    };
+};
+
+const { loaddata, main } = checkmm;
+
+checkmm.setLoaddata(createAsyncPerformanceMonitor(loaddata));
+const newMain = createAsyncPerformanceMonitor(main);
+checkmm.setMain(newMain);
+
+newMain(process.argv.slice(1)).then(exitCode => {
+    process.exitCode = exitCode;
+    console.log(JSON.stringify(timings, null, 2));
+});
