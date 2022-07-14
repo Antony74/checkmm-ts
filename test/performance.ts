09e27cc893ca875063ebc38b374307bd7d19f66d
Antony Bartlett <antony.bartlett@ukri.org>
2022-07-14 13:25:03 +0100

    Half-decent performance info

--- test/performance.ts
+++ test/performance.ts
@@ -9,6 +9,8 @@ interface Timing {
 
 const timings: { [name: string]: Timing } = {};
 
+type TimingPair = [name: string, timing: Timing];
+
 const createPerformanceMonitor = <RT, Args extends Array<unknown>>(fn: (...args: Args) => RT, _name?: string) => {
     const name: string = _name ?? fn.name;
 
@@ -55,12 +57,17 @@ const createAsyncPerformanceMonitor = <RT, Args extends Array<unknown>>(
     };
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const skipKnownFn = (label: string) => false;
+
 const isKnownAsyncFn = (label: string) =>
     label === 'loaddata' || label === 'main' || label === 'readfile' || label === 'readtokens';
 
 for (const label in checkmm) {
     if (typeof (checkmm as any)[label] === 'function') {
-        if (isKnownAsyncFn(label)) {
+        if (skipKnownFn(label)) {
+            // duly skipped
+        } else if (isKnownAsyncFn(label)) {
             (checkmm as any)[label] = createAsyncPerformanceMonitor((checkmm as any)[label]);
         } else {
             (checkmm as any)[label] = createPerformanceMonitor((checkmm as any)[label]);
@@ -68,7 +75,38 @@ for (const label in checkmm) {
     }
 }
 
+const sort = (compareFn?: (a: TimingPair, b: TimingPair) => number) => {
+    const arr: TimingPair[] = [];
+    for (const label in timings) {
+        arr.push([label, timings[label]]);
+    }
+
+    return arr.sort(compareFn);
+};
+
 checkmm.main(process.argv.slice(1)).then(exitCode => {
     process.exitCode = exitCode;
-    console.log(JSON.stringify(timings, null, 2));
+
+    const byCallCount: TimingPair[] = sort((a: TimingPair, b: TimingPair) => {
+        return b[1].callCount - a[1].callCount;
+    });
+    const millisecondsTaken: TimingPair[] = sort((a: TimingPair, b: TimingPair) => {
+        return b[1].millisecondsTaken - a[1].millisecondsTaken;
+    });
+
+    const padding = 60;
+
+    for (let n = 0; n < byCallCount.length; ++n) {
+        const label1 = byCallCount[n][0];
+        const callCount1 = byCallCount[n][1].callCount;
+        const taken1 = (byCallCount[n][1].millisecondsTaken / 1000).toFixed(2);
+        const label2 = millisecondsTaken[n][0];
+        const callCount2 = millisecondsTaken[n][1].callCount;
+        const taken2 = (millisecondsTaken[n][1].millisecondsTaken / 1000).toFixed(2);
+        console.log(
+            `${label1} called ${callCount1} times in ${taken1} seconds.`.padStart(padding, ' ') +
+                `${label2} called ${callCount2} times in ${taken2} seconds.`.padStart(padding, ' '),
+        );
+        console.log();
+    }
 });
